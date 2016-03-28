fd898df70fe7e03fb774714bb5fe8c13b86c4ac2
Jan Jorgensen <jandjorgensen@gmail.com>
2016-03-28 07:41:28 -0700

    0.1.0

---
+++ dist/cjs/can-view-href.js
@@ -0,0 +1,27 @@
+/*can-view-href@0.0.0#can-view-href*/
+var can = require('can/util/util');
+var expression = require('can/view/stache/expression');
+require('can/view/callbacks/callbacks');
+require('can/view/scope/scope');
+require('can/route/route');
+var removeCurly = function (value) {
+    if (value[0] === '{' && value[value.length - 1] === '}') {
+        return value.substr(1, value.length - 2);
+    }
+    return value;
+};
+can.view.attr('can-href', function (el, attrData) {
+    var attrInfo = expression.parse('tmp(' + removeCurly(el.getAttribute('can-href')) + ')', { baseMethodType: 'Call' });
+    var getHash = attrInfo.hash(attrData.scope, null);
+    var routeHref = can.compute(function () {
+        return can.route.url(getHash());
+    });
+    el.setAttribute('href', routeHref());
+    var handler = function (ev, newVal) {
+        el.setAttribute('href', newVal);
+    };
+    routeHref.bind('change', handler);
+    can.bind.call(el, 'removed', function () {
+        routeHref.unbind('change', handler);
+    });
+});
