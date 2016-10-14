e8ee44fb386024ef51307518fc5895e76ffacea8
Kevin Phillips <kphillips86@gmail.com>
2016-10-14 13:19:33 -0500

    3.0.0-pre.2

---
+++ dist/amd/can-view-href.js
@@ -0,0 +1,30 @@
+/*can-view-href@3.0.0-pre.1#can-view-href*/
+define(function (require, exports, module) {
+    var expression = require('can-stache/src/expression');
+    var viewCallbacks = require('can-view-callbacks');
+    var compute = require('can-compute');
+    var route = require('can-route');
+    require('can-util/dom/events/removed');
+    var domEvents = require('can-util/dom/events');
+    var removeCurly = function (value) {
+        if (value[0] === '{' && value[value.length - 1] === '}') {
+            return value.substr(1, value.length - 2);
+        }
+        return value;
+    };
+    viewCallbacks.attr('can-href', function (el, attrData) {
+        var attrInfo = expression.parse('tmp(' + removeCurly(el.getAttribute('can-href')) + ')', { baseMethodType: 'Call' });
+        var getHash = attrInfo.argExprs[0].value(attrData.scope, null);
+        var routeHref = compute(function () {
+            return route.url(getHash());
+        });
+        el.setAttribute('href', routeHref());
+        var handler = function (ev, newVal) {
+            el.setAttribute('href', newVal);
+        };
+        routeHref.on('change', handler);
+        domEvents.addEventListener.call(el, 'removed', function () {
+            routeHref.off('change', handler);
+        });
+    });
+});
